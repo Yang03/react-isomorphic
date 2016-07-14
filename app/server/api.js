acb4e44220eef410bfc50011f1d270486e134445
flashYang <flashyang@flashYangdeMacBook-Pro.local>
2016-07-14 15:58:29 +0800

    add getCategoryall

--- app/server/api.js
+++ app/server/api.js
@@ -3,50 +3,93 @@ import {encText, decText } from  './util/encrypt'
 import aes from 'aes-cross'
 import crypto from 'crypto'
 
+const baseUrl = ''
+const appKey = ''
+const appId = ''
+
 
 function sign() {
-    //console.log(Math.ceil(new Date().getTime()/ 1000))
-    //
-    const timestamp = Math.floor(new Date().getTime() / 1000)
-    const str = appId + timestamp
-    return encText(str, appKey)
+	const timestamp = Math.floor(new Date().getTime() / 1000)
+	const str = appId + timestamp
+	return encText(str, appKey)
 }
 
-//BGNmpkJrPz/hAxX6X9SRUs8PetvRW4B6kx0TtPLALO8=
 function buildQuery(obj) {
-    const keys = Object.keys(obj)
-    const arry = []
-    for (let i = 0, len = keys.length; i < len; ++i) {
-        let key = keys[i]
-        arry.push(key + '=' + encodeURIComponent(obj[key]))
-    }
-    return arry.join('&')
+	const keys = Object.keys(obj)
+	const arry = []
+	for (let i = 0, len = keys.length; i < len; ++i) {
+		let key = keys[i]
+		arry.push(key + '=' + encodeURIComponent(obj[key]))
+	}
+	return arry.join('&')
 }
 
-function getData(path, opts = {}) {
-    const defaultOpts = {
+async function getData(path, opts = {}) {
+	const defaultOpts = {
 			'appId': appId,
 			'appKey': appKey,
 			'timestamp': Math.floor(new Date().getTime() / 1000),
 			'sign': sign()
 		}
-    const queryOpts = Object.assign(opts, defaultOpts)
-    const queryStr = buildQuery(queryOpts)
-    const requestUrl = `${baseUrl}${path}?${queryStr}`
-    return axios.get(requestUrl)
+	const queryOpts = Object.assign(opts, defaultOpts)
+	const queryStr = buildQuery(queryOpts)
+	const requestUrl = `${baseUrl}${path}?${queryStr}`
+	return await axios.get(requestUrl)
 }
 
 export async function categoryAll(ctx, next) {
-    const response = await getData('/category/all')
-    if (response.code = '200') {
-        return response.data
-    }
-    return [];
-    //ctx.body = reponse.data
+	const response = await getData('/category/all')
+	let data = []
+	if (response.data.code = '200') {
+		data = formatCategory(response.data.data)
+	}
+	let results = await getCategoies(data)
+	let result = formatCategires(results)
+	data.forEach((value, index) => {
+		data[index].child = result[index]
+	})
+	ctx.body = data
+}
+
+ async function getCategoies(data) {
+	let promises = data.map((value) => getData('/manufacturer/category/', {'id': value.id}))
+	let results = [];
+	for (let promise of promises) {
+		results.push(await promise);
+	}
+	return results
 }
 
 function formatCategory (data) {
-    const arr = ['手机', '智能数码', '笔记本', '单反相机', '镜头', '平板电脑']
+	const arr = ['手机', '智能数码', '笔记本', '单反相机', '镜头', '平板电脑']
+	const brandList = data.filter((value, index) => {
+		return arr.indexOf(value.name) >= 0
+	})
+	return brandList
+}
 
 
+function formatCategires (data) {
+	let arr = []
+	data.map((value, index) => {
+		if (value.data.code == 200) {
+			let temp = sortBykey(value.data.data, 'order')
+			arr[index] = temp
+		} else {
+			arr[index] = []
+		}
+	})
+	return arr
+}
+
+function sortBykey(arr, key) {
+	return arr.sort(function(a, b){
+		let x = a[key], y = b[key]
+		if (x < y) {
+			return -1
+		} else if (x > y) {
+			return 1
+		}
+		return 0
+	})
 }
--- app/server/index.js
+++ app/server/index.js
@@ -1,30 +1,55 @@
 import Koa from 'koa'
+import json from 'koa-json'
+import serve from 'koa-static'
 import Router from 'koa-router'
 import compose from 'koa-compose'
 import convert from 'koa-convert'
-import json from 'koa-json'
 import favicon from 'koa-favicon'
+import { categoryAll } from './api'
 import bodyParser from 'koa-bodyparser'
 import notFound from './middlewares/notFound'
-import { categoryAll } from './api'
+import devConfig from '../client/webpack.dev'
+import webpack from 'webpack'
+import {devMiddleware, hotMiddleware} from 'koa-webpack-middleware'
 
+const dev = process.env.NODE_ENV != 'production'
 
 const app = new Koa()
 const router = Router()
 
+const compile = webpack(devConfig)
+if (dev) {
+	app.use(devMiddleware(compile, {
+		noInfo: true,
+		reload: true,
+		publicPath: devConfig.output.publicPath,
+		stats: {
+			colors: true
+		}
+}))
+
+app.use(hotMiddleware(compile, {
+	log: console.log,
+	path: '/dist/__webpack_hmr',
+	heartbeat: 5000
+	}))
+}
+
+app.use(serve(__dirname + '../../client/'))
+
 router
-    .get('/categoryAll', categoryAll)
+	.get('/categoryAll', categoryAll)
 
 const middlewares = compose([
-	// favicon(__dirname + '/favicon.ico'),
-	// bodyParser({formLimit: '5mb'}),
-    notFound(),
-    json(),
-    router.routes(),
+	favicon(__dirname + '/favicon.ico'),
+	bodyParser({formLimit: '5mb'}),
+	notFound(),
+	json(),
+	router.routes(),
 	router.allowedMethods()
 ])
 
-//app.use(json())
+
 app.use(middlewares)
 
 app.listen('3000', () => console.log('server started 3000'))
